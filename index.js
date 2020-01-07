3e690dd6d7a2463172bb87fae5d4041f26c1c32d
NiaChoi <y.nia.choi@gnmail.com>
2020-01-07 15:47:29 +0900

    update example 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
-const _webPort = 1004;
+const _webPort = 80;
 global.app = express();
 app.use(cors());
 global.router = express.Router();
@@ -12,6 +12,11 @@ app.use("/", router);
 router.get("/", (req, res) => {
     res.sendFile(`${path}/web-client/index.html`);
 });
+
+router.get("/app.js",(req, res) => {
+    res.sendFile(`${path}/web-client/app.js`);
+});
+
 
 app.listen(_webPort, () => {
     console.log(`web server running on ${_webPort}`);
